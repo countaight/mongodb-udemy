1d7f61d7fc3e89cf4254be540a63f5ee09e4aa0d
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2017-06-29 13:55:37 -0500

    Scaffold of reading test with a function to set up a user before every test

---
+++ users/test/reading_test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+const User = require('../src/user.');
+
+describe('Reading users out of the database', () => {
+	// Declare variable outside the beforeEach function so that it's available to the rest of the it test functions.
+	let joe;
+
+	beforeEach(() => {
+		joe = new User({ name: 'Joe'});
+		joe.save()
+			.then(() => done());
+	});
+
+	it('finds all users with a name of Joe', () => {
+
+	});
+})
