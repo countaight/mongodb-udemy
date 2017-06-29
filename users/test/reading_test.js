80f2384d2da5648f5da262ad54b0a482f519b676
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2017-06-29 14:01:03 -0500

    Add find query and confirm with console log that an array is returned with a user of Joe

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -1,18 +1,22 @@
 const assert = require('assert');
 
-const User = require('../src/user.');
+const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
 	// Declare variable outside the beforeEach function so that it's available to the rest of the it test functions.
 	let joe;
 
-	beforeEach(() => {
+	beforeEach((done) => {
 		joe = new User({ name: 'Joe'});
 		joe.save()
 			.then(() => done());
 	});
 
-	it('finds all users with a name of Joe', () => {
-
+	it('finds all users with a name of Joe', (done) => {
+		User.find({ name: 'Joe'})
+			.then((users) => {
+				console.log(users);
+				done();
+			});
 	});
 })
