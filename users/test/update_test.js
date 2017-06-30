67124c4c428c7b3d44a65d456de9d4cc658771b0
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2017-06-30 14:07:18 -0500

    Create update test boiler plate and update test file names to match

---
+++ users/test/update_test.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+
+const User = require('../src/user');
+
+describe('Updating records', () => {
+	let joe;
+
+	beforeEach((done) => {
+		joe = new User({ name: 'Joe' });
+		joe.save()
+			.then(() => done());
+	});
+
+	it('instance type using set n save', () => {
+
+	});
+
+	it('instance type using update', () => {
+
+	});
+
+	it('', () => {
+
+	});
+
+	it('', () => {
+
+	});
+
+	it('', () => {
+
+	});
+
+
+});
