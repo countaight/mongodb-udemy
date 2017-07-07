2c8cc23dd3f399efe998d1e3e4c9e4aab097398b
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2017-07-07 11:18:28 -0500

    Implement full search features including by name, age range, and yearsActive range

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -9,7 +9,7 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
-	const query = Artist.find({})
+	const query = Artist.find(buildQuery(criteria))
 		.sort({ [sortProperty]: 1 })
 		.skip(offset)
 		.limit(limit);
@@ -23,4 +23,15 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
 				limit
 			};
 		});
+};
+
+const buildQuery = (criteria) => {
+	const { name, age, yearsActive } = criteria;
+	const query = { name: { $regex: `${name}`, $options: 'i' } };
+	if (age) { query.age = { $gte: age.min, $lte: age.max }; }
+	if (yearsActive) {
+		query.yearsActive = { $gte: yearsActive.min, $lte: yearsActive.max };
+	}
+
+	return query;
 };
