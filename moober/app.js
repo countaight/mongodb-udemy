2692d87e5e816986ffd1c46e405d5824ff287d10
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2017-07-07 15:21:16 -0500

    Fix mongoose and db issues to ensure the create test is accurate

--- moober/app.js
+++ moober/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const routes = require('./routes/routes');
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/moober');
 
 app.use(bodyParser.json());
 routes(app);
